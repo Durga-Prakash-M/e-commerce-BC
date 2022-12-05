a7dcd260166f5d4df290f439f317d6746ca6e468
Durga-Prakash-M <durgaprakashmadishetty@gmail.com>
2022-12-05 15:22:43 +0530

    added exercise-3

--- src/components/IndivdualProduct.js
+++ src/components/IndivdualProduct.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import useProductData from "../hooks/useProductData";
 import "../styles/IndividualProductStyles.css";
+
+const quantityMessage = (quantity) => {
+  if (quantity === 0) return "unavailable";
+  else if (quantity <= 10) return "selling fast";
+  return "available";
+};
+
 const IndivdualProduct = () => {
   const { id } = useParams();
   const { isLoading, data } = useProductData(id);
+
+  const [image, setImage] = useState("");
+
+  useEffect(() => {}, [image]);
+  useEffect(() => {
+    if (data) setImage(data.data.image);
+  }, [data]);
+
   if (isLoading) {
     return <div>Loading product....</div>;
   }
@@ -12,7 +27,7 @@ const IndivdualProduct = () => {
   return (
     <div className="IndividualProduct">
       <div className="prod-img">
-        <img src={product.image} alt="product" />
+        <img src={image} alt="product" />
       </div>
       <div className="details">
         <h2>{product.name}</h2>
@@ -20,11 +35,22 @@ const IndivdualProduct = () => {
           Price: <strong>${product.price}</strong>
         </p>
         <p>Description: {product.description}</p>
-        <p>
-          {product.quantity === 0
-            ? "Out of Stock"
-            : "Quantity: " + product.quantity}
-        </p>
+        <p>{quantityMessage(product.quantity)}</p>
+        variants:
+        <div className="variants">
+          {product.variants.map((variant) => {
+            return (
+              <div
+                key={variant.color}
+                className="variant"
+                onClick={() => {
+                  setImage(variant.image);
+                }}
+                style={{ backgroundColor: `${variant.color}` }}
+              ></div>
+            );
+          })}
+        </div>
         <div className="add-to-cart-button">Add to Cart</div>
       </div>
     </div>
